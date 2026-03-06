89a06cf35e40f4e608118874ff999f89b06b082d
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2026-03-06 12:38:17 -0300

    Initial commit

---
+++ Domain/Entities/EstacaoRecarga.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class EstacaoRecarga(string identificador, decimal potenciaKw)
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Identificador { get; private set; } = identificador;
+        public decimal PotenciaKw { get; private set; } = potenciaKw;
+        public bool EstaOcupada { get; private set; }
+        public bool EmManutencao { get; private set; }
+
+
+        // DDD A ENTIDADE DEVE SE AUTOVALIDAR, ENTÃO A LÓGICA DE VALIDAÇÃO DEVE ESTAR AQUI
+        public void IniciarRecarga()
+        {
+            if (EmManutencao)
+            {
+                throw new DomainException("Não é possivel iniciar a recarga em uma estação em manutenção.");
+            }
+            if (EstaOcupada)
+            {
+                throw new DomainException("Estação já está em uso.");
+            }
+        }
+    }
+}
