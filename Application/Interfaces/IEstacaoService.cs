e737ad266fcac78c919376754c49733356e97ce1
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2026-03-06 14:31:59 -0300

    Service criado com sucesso, aplicando o encapsulamento(Utilizando metodos)

---
+++ Application/Interfaces/IEstacaoService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface IEstacaoService
+    {
+        Task CriarEstacaoAsync(CriarEstacaoRequest request);
+        Task IniciarRecargaAsync(Guid estacaoId);
+    }
+}
---
+++ Application/Services/EstacaoService.cs
@@ -0,0 +1,33 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Domain.Entities;
+using Domain.Exceptions;
+using Domain.Interfaces;
+
+namespace EcoCharge.Application.Services;
+
+public class EstacaoService(
+    IEstacaoRepository repository,
+    IUnitOfWork uow) : IEstacaoService
+{
+    public async Task CriarEstacaoAsync(CriarEstacaoRequest request)
+    {
+        var estacao = new EstacaoRecarga(request.Identificador, request.PotenciaKw);
+
+        await repository.AdicionarAsync(estacao);
+        await uow.CommitAsync();
+    }
+
+    public async Task IniciarRecargaAsync(Guid estacaoId)
+    {
+        var estacao = await repository.ObterPorIdAsync(estacaoId);
+
+        if (estacao == null)
+            throw new DomainException("Estação de recarga não encontrada.");
+
+        estacao.IniciarRecarga();
+
+        repository.Atualizar(estacao);
+        await uow.CommitAsync();
+    }
+}
